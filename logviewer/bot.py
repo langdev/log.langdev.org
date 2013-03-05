23736435c52afc4128563cae0b632d043848a2d0
Eunchong Yu <kroisse@gmail.com>
2013-03-05 22:07:29 +0900

    Massively refactor bot.py
    for testing without app.config all the cases

--- logviewer/bot.py
+++ logviewer/bot.py
@@ -13,8 +13,7 @@ import requests
 import tornado.ioloop
 import tornado.iostream
 import tornadio2
-
-from .app import app
+from flask import current_app
 
 
 _log = logging.getLogger(__name__)
@@ -35,22 +34,22 @@ action.handlers = []
 
 
 @action(r'^PING :(.+)')
-def pong(stream, m):
-    send_line(stream, 'PONG :' + m.group(1))
+def pong(bot, m):
+    bot.send_line('PONG :' + m.group(1))
 
 
 @action(r'^:(.+?) 001')
-def join_channel(stream, m):
+def join_channel(bot, m):
     logger = _log.getChild('join_channel')
-    for channel in app.config['IRC_CHANNELS']:
-        send_line(stream, 'JOIN ' + channel)
+    for channel in current_app.config['IRC_CHANNELS']:
+        bot.send_line('JOIN ' + channel)
         logger.info('Joining channel: %s', channel)
 
 
 @action(r'^\:(.+)\!(.+) PRIVMSG \#(.+) :(.+)')
-def update(stream, m):
+def update(bot, m):
     ChatConnection.emit_all('update')
-    check_link(stream, m)
+    check_link(bot, m)
 
 
 def check_link(stream, m):
@@ -60,7 +59,7 @@ def check_link(stream, m):
     if url_m:
         url = url_m.group(0)
         ts = time.time()
-        auth = hashlib.md5(app.config['LANGDEV_LINKS_API_KEY'] + str(int(ts)) +
+        auth = hashlib.md5(current_app.config['LANGDEV_LINKS_API_KEY'] + str(int(ts)) +
                            hashlib.md5(url).hexdigest()).hexdigest()
         data = {
             'ts': ts,
@@ -80,69 +79,77 @@ def get_timestamp(dt):
     return dt.isoformat().split('.', 2)[0]
 
 
-log_file = None
-logfile_path = os.path.join(app.config['LOG_DIR'], 'langdev.log')
-try:
-    today = datetime.datetime.fromtimestamp(os.path.getmtime(logfile_path))
-except OSError:
-    today = datetime.datetime.today()
+class LogWriter(object):
+    def __init__(self, log_dir):
+        self.log_file = None
+        self.logfile_path = os.path.join(log_dir, 'langdev.log')
+        try:
+            mtime = os.path.getmtime(self.logfile_path)
+            self.today = datetime.datetime.fromtimestamp(mtime)
+        except OSError:
+            self.today = datetime.datetime.today()
+
+    def rotate_log_file(self):
+        if self.log_file:
+            self.log_file.close()
+        self.log_file = None
+        os.rename(self.logfile_path,
+                  self.logfile_path + '.' + get_date_str(self.today))
+
+    def get_log_file(self, date):
+        if self.today.date() != date.date():
+            self.rotate_log_file()
+            self.today = date
+        if not self.log_file:
+            self.log_file = io.open(self.logfile_path, 'a', encoding='utf-8')
+        return self.log_file
+
+    def log(self, text):
+        now = datetime.datetime.now()
+        msg = '[{0}] {1}'.format(get_timestamp(now), text)
+        logfile = self.get_log_file(now)
+        print >> logfile, msg
+        logfile.flush()
 
 
-def rotate_log_file():
-    global log_file
-    if log_file:
-        log_file.close()
-    log_file = None
-    os.rename(logfile_path, logfile_path + '.' + get_date_str(today))
+class Bot(object):
+    def __init__(self, logger):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        self.stream = tornado.iostream.IOStream(sock)
+        self.logger = logger
 
+    def connect(self, host, port):
+        logger = _log.getChild('send_request')
 
-def get_log_file(date):
-    global today, log_file
-    if today.date() != date.date():
-        rotate_log_file()
-        today = date
-    if not log_file:
-        log_file = io.open(logfile_path, 'a', encoding='utf-8')
-    return log_file
+        def logged_request():
+            logger.info('Socket connected: %s:%s' % (host, port))
+            self.send_request()
+        self.stream.connect((host, port), logged_request)
 
+    def send_request(self):
+        self.send_line('USER nakji 0 * :fisher')
+        self.send_line('NICK ' + current_app.config['IRC_NICKNAME'])
+        self.stream.read_until(b'\r\n', self.receive_line)
 
-def log(text):
-    now = datetime.datetime.now()
-    msg = '[{0}] {1}'.format(get_timestamp(now), text)
-    logfile = get_log_file(now)
-    print >> logfile, msg
-    logfile.flush()
+    def receive_line(self, line):
+        if not line:
+            return
+        line = line.decode('utf-8')
+        self.logger.log('<<< ' + line)
+        for pattern, handler in action.handlers:
+            match = pattern.match(line)
+            if match:
+                handler(self, match)
+        self.stream.read_until(b'\r\n', self.receive_line)
 
-
-def send_line(stream, line):
-    log('>>> ' + line)
-    stream.write(line.encode('utf-8') + b'\r\n')
-
-
-def receive_line(line):
-    global stream
-    if not line:
-        return
-    line = line.decode('utf-8')
-    log('<<< ' + line)
-    for pattern, handler in action.handlers:
-        match = pattern.match(line)
-        if match:
-            handler(stream, match)
-    stream.read_until(b'\r\n', receive_line)
-
-
-def send_request():
-    global stream
-    logger = _log.getChild('send_request')
-    logger.info('Socket connected: %s:%s' % (host, port))
-    send_line(stream, 'USER bot 0 * :fishing')
-    send_line(stream, 'NICK ' + app.config['IRC_NICKNAME'])
-    stream.read_until(b'\r\n', receive_line)
+    def send_line(self, line):
+        self.logger.log('>>> ' + line)
+        self.stream.write(line.encode('utf-8') + b'\r\n')
 
 
 class ChatConnection(tornadio2.SocketConnection):
     connections = set()
+    bot = None
 
     def on_open(self, request):
         logger = _log.getChild(self.__class__.__name__ + '.on_open')
@@ -156,10 +163,10 @@ class ChatConnection(tornadio2.SocketConnection):
 
     @tornadio2.conn.event
     def msg(self, nick, msg):
-        channel = app.config['IRC_CHANNELS'][0]
+        channel = current_app.config['IRC_CHANNELS'][0]
         message = '<{0}> {1}'.format(nick, msg)
         message = message.replace('\r\n', ' ')
-        send_line(stream, 'PRIVMSG {0} :{1}'.format(channel, message))
+        self.bot.send_line('PRIVMSG {0} :{1}'.format(channel, message))
         self.emit('update')
 
     @classmethod
@@ -168,16 +175,27 @@ class ChatConnection(tornadio2.SocketConnection):
             conn.emit(event, *args, **kwargs)
 
 
-if __name__ == '__main__':
-    _log.setLevel(logging.INFO)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-    stream = tornado.iostream.IOStream(sock)
-    host, port = app.config['IRC_HOST'], app.config['IRC_PORT']
-    stream.connect((host, port), send_request)
+def launch_bot(config):
+    logger = LogWriter(config['LOG_DIR'])
+    host, port = config['IRC_HOST'], config['IRC_PORT']
+    bot = Bot(logger=logger)
+    bot.connect(host, port)
+    return bot
 
+
+def launch_chatserver(bot):
+    ChatConnection.bot = bot
     chat_router = tornadio2.TornadioRouter(ChatConnection)
     application = tornado.web.Application(chat_router.urls,
                                           socket_io_port=8888)
     socketio_server = tornadio2.SocketServer(application, auto_start=False)
     socketio_server.start()
-    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    _log.setLevel(logging.INFO)
+    from .app import app
+    with app.app_context():
+        bot = launch_bot(current_app.config)
+        launch_chatserver(bot)
+        tornado.ioloop.IOLoop.instance().start()
