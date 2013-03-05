c1f083c51c7d4a82612ed36eb33c8ba036612189
Eunchong Yu <kroisse@gmail.com>
2013-03-05 22:03:37 +0900

    Fix dependencies

--- setup.py
+++ setup.py
@@ -35,9 +35,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'eventlet',
         'requests',
-        'flask',
+        'flask >= 0.9',
         'pytz',
         'tornadio2',
     ],
