3ba19ed4b28562a4e590ece69f4e222cd3cd77e1
Eunchong Yu <kroisse@gmail.com>
2013-08-08 17:31:02 +0900

    Add Google Apps OAuth2 auth backend

---
+++ logviewer/auth/googleapps.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from flask import request, url_for
+from flask.ext.oauthlib.client import OAuth, OAuthException
+import requests
+
+from ..exc import AuthenticationError
+
+oauth = OAuth()
+
+
+class GoogleAppsAuth(object):
+    def __init__(self, name, domain, consumer_key, consumer_secret):
+        self.domain = domain
+        self.remote = oauth.remote_app(
+            name,
+            consumer_key=consumer_key,
+            consumer_secret=consumer_secret,
+            base_url=u'https://www.googleapis.com/',
+            request_token_url=None,
+            request_token_params={'scope': [
+                u'https://www.googleapis.com/auth/userinfo.email',
+            ]},
+            access_token_url=u'https://accounts.google.com/o/oauth2/token',
+            access_token_method='POST',
+            authorize_url=u'https://accounts.google.com/o/oauth2/auth',
+        )
+        self.authenticate = self.remote.authorized_handler(self._authorize)
+
+    def login(self, error=None):
+        callback_url = url_for('authenticate',
+                               next=request.args.get('next'),
+                               _external=True)
+        return self.remote.authorize(callback=callback_url)
+
+    def _authorize(self, resp):
+        if resp is None:
+            raise AuthenticationError(
+                'Access denied: '
+                'reason={0.error_reason} error={0.error_description}'
+                .format(request.args))
+        elif isinstance(resp, OAuthException):
+            error = AuthenticationError()
+            error.__cause__ = resp
+            raise error
+        id_token = _decode_jwt(resp['id_token'])
+        username, domain = id_token['email'].rsplit('@', 1)
+        if domain != self.domain:
+            raise AuthenticationError("user doesn't in the proper group")
+        return {'username': username}
+
+
+def _decode_jwt(token):
+    r = requests.post('https://www.googleapis.com/oauth2/v1/tokeninfo',
+                      data={'id_token': token})
+    return r.json()
