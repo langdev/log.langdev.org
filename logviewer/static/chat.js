f548bf76b19e076060ed1af9208865913bae5a42
Kim Taeho <dittos@gmail.com>
2013-08-30 23:12:32 +0900

    reindent

--- logviewer/static/chat.js
+++ logviewer/static/chat.js
@@ -1,30 +1,30 @@
 function ChatSession(options) {
-	this.options = options;
-	this.from = options.from || 0;
-	this.onUpdate = options.onUpdate || function(){};
+    this.options = options;
+    this.from = options.from || 0;
+    this.onUpdate = options.onUpdate || function(){};
 }
 
 ChatSession.prototype = {
-	start: function() {
+    start: function() {
         this.socket = io.connect(this.options.url);
         var self = this;
         this.socket.on('update', function() {
-        	self.update(self.onUpdate);
+            self.update(self.onUpdate);
         });
     },
 
     send: function(msg) {
         this.socket.emit('msg', {
-        	nick: this.options.nickname,
-        	channel: this.options.channel,
-        	msg: msg
+            nick: this.options.nickname,
+            channel: this.options.channel,
+            msg: msg
         });
     },
     
     update: function(next) {
-    	var self = this;
-    	$.getJSON('?from=' + this.from, function(data) {
-    		if (!data.html) return;
+        var self = this;
+        $.getJSON('?from=' + this.from, function(data) {
+            if (!data.html) return;
             self.from = data.last_no + 1;
             if (next) next.call(self, data.html);
         });
@@ -32,11 +32,11 @@ ChatSession.prototype = {
 };
 
 function LogView(el) {
-	this.el = el;
+    this.el = el;
 }
 
 LogView.prototype = {
-	append: function(data) {
+    append: function(data) {
         this.el.append(data);
         apply_noreferrer(this.el);
     },
@@ -53,34 +53,34 @@ LogView.prototype = {
 };
 
 function ChatController(options) {
-	this.options = options;
-	this.session = options.session;
-	this.session.onUpdate = $.proxy(this.onUpdate, this);
+    this.options = options;
+    this.session = options.session;
+    this.session.onUpdate = $.proxy(this.onUpdate, this);
 
     var self = this;
-	this.options.startChatView.on('click', function() {
-		self.startChat();
-		return false;
+    this.options.startChatView.on('click', function() {
+        self.startChat();
+        return false;
     });
     this.options.inputFormView.on('submit', function() {
-    	self.sendMessage();
-    	return false;
+        self.sendMessage();
+        return false;
     });
 }
 
 ChatController.prototype = {
-	startChat: function() {
-    	this.options.startChatView.slideUp();
-    	this.options.inputFormView.slideDown();
-    	var self = this;
+    startChat: function() {
+        this.options.startChatView.slideUp();
+        this.options.inputFormView.slideDown();
+        var self = this;
 
         this.normalTitle = document.title;
-    	observeWindowVisibility(function(visible) {
+        observeWindowVisibility(function(visible) {
             self.setWindowVisible(visible);
         });
 
-    	this.session.update(function(data) {
-    		self.appendLog(data);
+        this.session.update(function(data) {
+            self.appendLog(data);
             self.options.logView.scrollToBottom();
         });
         this.session.start();
@@ -88,35 +88,35 @@ ChatController.prototype = {
 
     onUpdate: function(data) {
         var willScroll = this.options.logView.shouldScrollToBottom();
-    	this.appendLog(data);
-    	if (willScroll)
-    		this.options.logView.scrollToBottom();
-    	if (!this._windowVisible)
-    		this.notifyPendingLog();
+        this.appendLog(data);
+        if (willScroll)
+            this.options.logView.scrollToBottom();
+        if (!this._windowVisible)
+            this.notifyPendingLog();
     },
 
     appendLog: function(data) {
-    	this.options.logView.append(data);
+        this.options.logView.append(data);
     },
     
     sendMessage: function() {
-    	var msg = this.options.inputView.val();
-    	if (msg) {
-    		this.session.send(msg);
-    		this.options.inputView.val('');
+        var msg = this.options.inputView.val();
+        if (msg) {
+            this.session.send(msg);
+            this.options.inputView.val('');
         }
     },
 
     setWindowVisible: function(visible) {
-    	if (!this._windowVisible && visible) {
-    		// Become visible
-    		document.title = this.normalTitle;
+        if (!this._windowVisible && visible) {
+            // Become visible
+            document.title = this.normalTitle;
         }
-    	this._windowVisible = visible;
+        this._windowVisible = visible;
     },
 
     notifyPendingLog: function() {
-    	document.title = '+ ' + this.normalTitle;
+        document.title = '+ ' + this.normalTitle;
     }
 };
 
@@ -137,7 +137,7 @@ ChatController.prototype = {
     }
 
     exports.isWindowVisible = function() {
-    	return !document[hidden];
+        return !document[hidden];
     };
 
     exports.observeWindowVisibility = function(callback) {
@@ -145,7 +145,7 @@ ChatController.prototype = {
             typeof hidden !== "undefined") {
             callback(exports.isWindowVisible());
             document.addEventListener(visibilityChange, function() {
-            	callback(exports.isWindowVisible());
+                callback(exports.isWindowVisible());
             }, false);
         }
     };
