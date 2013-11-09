2efdbe8db1c821fa440fc08e37ae1b2a09ba064c
Taeho Kim <dittos@gmail.com>
2013-11-09 12:45:55 +0900

    Integrate flags into navbar

--- logviewer/static/flag.js
+++ logviewer/static/flag.js
@@ -1,45 +1,16 @@
-var flag_mode = false;
 var flags = [];
-function toggle_flag_mode(mode) {
-    flag_mode = !flag_mode;
-    if (flag_mode) {
-    	$('.time a').addClass('flaggable').click(function () {
-    		var time = $(this).text();
-    		var line = $(this).closest('tr').attr('id').substring(4);
-    		var title = prompt('Enter flag title');
-    		if (title != null)
-    			$.post(location.pathname + '/' + line + '/flags', {time: time, title: title},
-    			    function(id) {
-                    	add_flag({line: line, time: time, title: title, id: id, user: nickname});
-                    });
-        });
-    } else {
-    	$('.time a').removeClass('flaggable').unbind('click');
-    }
-
-    var btn = $('#toggle-flag-mode');
-    var desc = $('#flag-mode-desc');
-    if (!flag_mode) {
-        btn.text('깃발 꽂기');
-        desc.hide();
-    } else {
-        btn.text('깃발 그만 꽂기');
-        desc.show();
-    }
-}
 function add_flag(item) {
     flags.push(item);
     flags.sort(function(a, b) { return parseInt(a.line) - parseInt(b.line); });
     redraw_flags();
 }
 function redraw_flags() {
-	$('#flags ul').html('');
+	$('#flags').html('');
 	if (flags.length > 0)
         $.each(flags, function (i, item) {
-            $('#flags ul').append('<li>' + item.time + ' <a href="' + location.pathname + '#line' + item.line + '">' + item.title + '</a></li>');
+            $('#flags').append('<li><a href="' + location.pathname + '#line' + item.line + '"><span class="time">' + item.time + '</span> ' + item.title + '</a></li>');
         });
-    else
-    	$('#flags ul').html('<li>아직 깃발이 하나도 없네요.</li>');
+    $('#flags').append('<li id="add-flag-link"><a href="#">깃발 추가...</a></li>');
 }
 function refresh_flags() {
 	$.getJSON(location.pathname + '/flags', function (data) {
@@ -47,14 +18,38 @@ function refresh_flags() {
 		redraw_flags();
     });
 }
-$(function() {
-	$('#toggle-flag-mode').click(function () {
-		toggle_flag_mode();
-		return false;
+
+var inFlagMode = false;
+function beginFlagMode() {
+    if (inFlagMode) return;
+    inFlagMode = true;
+    $('#flag-mode-desc').fadeIn('fast');
+    $('.time a').addClass('flaggable');
+    $('#content').on('click', '.time a', function() {
+        var time = $(this).text();
+        var line = $(this).closest('tr').attr('id').substring(4);
+        var title = prompt('Enter flag title');
+        if (title)
+            $.post(location.pathname + '/' + line + '/flags', {time: time, title: title},
+                function(id) {
+                    add_flag({line: line, time: time, title: title, id: id, user: window.NICKNAME});
+                    alert('추가됨');
+                });
+        return false;
     });
-	$('#refresh-flags').click(function () {
-		refresh_flags();
-		return false;
+}
+function endFlagMode() {
+    if (!inFlagMode) return;
+    inFlagMode = false;
+    $('#flag-mode-desc').fadeOut('fast');
+    $('.time a').removeClass('flaggable');
+    $('#content').off('click', '.time a');
+}
+$(function() {
+    $('#flags').on('click', '#add-flag-link', function() {
+        beginFlagMode();
+        $('#flags').dropdown('toggle'); // hide dropdown
+        return false;
     });
     refresh_flags();
 });
